14ce7b6d8fc06155d2b7cd0d9cdc776e5995e93f
sophie <m.n.sophie@outlook.com>
2024-08-06 21:25:32 +0300

    lil update

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("43mrJ7631xbt5DZGJemvk5TqSR3g4EMjxBd3Sh2BdukJ");
 
 #[program]
 pub mod nft_lend_borrow {
